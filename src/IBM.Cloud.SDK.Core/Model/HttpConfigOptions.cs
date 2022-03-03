2b717c151c6a0a833e755a21a849bdd3eba57164
Kevin Kowalski <kevin.kowalski@ibm.com>
2022-03-03 09:13:52 -0600

    fix(ibm-service): add httpconfigs to set a proxy and disable ssl

---
+++ src/IBM.Cloud.SDK.Core/Model/HttpConfigOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace IBM.Cloud.SDK.Core.Model
+{
+    public class HttpConfigOptions
+    {
+        public static string ErrorMessagePropMissing = "The {0} property is required but was not specified.";
+        public WebProxy Proxy { get; private set; }
+        public bool DisableSslVerification { get; private set; }
+
+        // this empty constructor will be used by the builder 
+        public HttpConfigOptions()
+        {
+        }
+
+        public HttpConfigOptions SetProxy(WebProxy proxy)
+        {
+            Proxy = proxy;
+            return this;
+        }
+
+        public HttpConfigOptions SetDisableSslVerification(bool disableSslVerification)
+        {
+            DisableSslVerification = disableSslVerification;
+            return this;
+        }
+
+        public HttpConfigOptions Build()
+        {
+            return this;
+        }
+    }
+}
